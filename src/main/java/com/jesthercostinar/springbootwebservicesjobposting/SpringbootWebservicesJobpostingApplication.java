9bf61967da6e6a6654b622deb028cdf51ec4be28
Jesther Costinar <jesther.jc15@gmail.com>
2023-06-06 13:33:38 +0800

    first commit

---
+++ src/main/java/com/jesthercostinar/springbootwebservicesjobposting/SpringbootWebservicesJobpostingApplication.java
@@ -0,0 +1,13 @@
+package com.jesthercostinar.springbootwebservicesjobposting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebservicesJobpostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebservicesJobpostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesthercostinar/springbootwebservicesjobposting/SpringbootWebservicesJobpostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesthercostinar.springbootwebservicesjobposting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebservicesJobpostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
